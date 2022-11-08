c94b5b2679242204313ea7ac8a4eb5f0b7ba65bf
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-11-09 00:28:08 +0300

    Add files via upload

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version=`wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name"`
-if ! sui --version | grep -q $version; then
+if ! sui --version 2&>/dev/null | grep -q $version; then
 	wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | tar -xzf -
 fi
 ./sui-node "$@"
